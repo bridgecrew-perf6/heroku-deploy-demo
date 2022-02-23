8e544d2a9d9d2bb97ecac43db7b95efdd043f136
Dan Katzuv <31829093+katzuv@users.noreply.github.com>
2022-02-23 21:23:10 +0200

    Update app.py

--- code/app.py
+++ code/app.py
@@ -1,10 +1,14 @@
 from datetime import datetime
+import os
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+_TOKEN = os.getenv("TOKEN")
+
+
 @app.get("/")
 async def root():
-    return {"message": f"Hello World! This is the staging environment. The time now is {datetime.now()}"}
+    return {"message": f"Hello World! This is the staging environment. The time now is {datetime.now()}. The token is {_TOKEN}"}
