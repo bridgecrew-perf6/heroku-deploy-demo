426e6001811c11b062b4b112a1d5d069c728bddd
Dan Katzuv <31829093+katzuv@users.noreply.github.com>
2022-02-24 01:11:54 +0200

    Update app.py

--- code/app.py
+++ code/app.py
@@ -6,9 +6,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-_TOKEN = os.getenv("TOKEN")
+_TOKEN = os.getenv("FIREBASE_API_KEY")
+_TOKEN2 = os.getenv("HD_FIREBASE_API_KEY")
 
 
 @app.get("/")
 async def root():
-    return {"message": f"Hello World! This is the staging environment. The time now is {datetime.now()}. The token is {_TOKEN}"}
+    return {"message": f"Hello World! This is the staging environment. The time now is {datetime.now()}. The token is {{_TOKEN}} or {{_TOKEN2}}"}
